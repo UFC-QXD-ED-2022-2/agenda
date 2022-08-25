a067d289ac52222693b109a4c1491abaa7afe9f5
Arthur Araruna <ararunaufc@gmail.com>
2022-08-25 12:40:16 -0300

    Início da leitura de opções via terminal

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 typedef struct contato {
   char nome[100];
   char numero[9];
@@ -6,6 +8,15 @@ typedef struct contato {
 int main() {
   Contato agenda[10];
   int quantidade = 0;
+
+  int opcao = 0;
+
+  do {
+    printf("Digite a opção desejada:\n");
+    printf("\t[0]: Sair\n");
+
+    scanf("%d", &opcao);
+  } while (opcao != 0);
 
   return 0;
 }
