c125c05e7ea50e21c05630ebc849237500e5e4e9
Arthur Araruna <ararunaufc@gmail.com>
2022-08-25 12:52:11 -0300

    Busca contato pelo nome.
    Precisa passar (potencialmente) por todos os contatos, pois não temos informações
    sobre a disposição deles na agenda para permitir encerrar a busca antes
    (a não ser quando encontramos o buscado).
    
    Complexidade: O(n)

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct contato {
   char nome[100];
@@ -16,6 +17,7 @@ int main() {
     printf("\t[0]: Sair\n");
     printf("\t[1]: Inserir contato\n");
     printf("\t[2]: Remover contato\n");
+    printf("\t[3]: Buscar contato por nome\n");
 
     scanf("%d", &opcao);
 
@@ -49,6 +51,22 @@ int main() {
 
       agenda[posicao] = agenda[quantidade - 1];
       --quantidade;
+    } else if (opcao == 3) {
+      printf("[Buscar contato pelo nome]\n");
+      char nome[100];
+
+      printf("Informe o nome para buscar: ");
+      scanf(" %s", nome);
+
+      int i;
+      for (i = 0; i < quantidade; ++i) {
+        if (strcmp(nome, agenda[i].nome) == 0) {
+          printf("Contato [%d]:\n", i);
+          printf("\tNome: %s\n", agenda[i].nome);
+          printf("\tNumero: %s\n\n", agenda[i].numero);
+          break;
+        }
+      }
     }
   } while (opcao != 0);
 
